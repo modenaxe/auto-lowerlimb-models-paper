36cd4127b8d8838ef5fd70b329e4e51d69d1c264
Luca Modenese <l.modenese@imperial.ac.uk>
2020-06-20 15:13:26 +0100

    comments

--- support_functions/attachPatellaGeom.m
+++ support_functions/attachPatellaGeom.m
@@ -15,14 +15,30 @@
 %    Author:   Luca Modenese,  2020                                       %
 %    email:    l.modenese@imperial.ac.uk                                  %
 % ----------------------------------------------------------------------- %
+% This script adds the patella to the model for the purposes of this 
+% Modenese-Renault paper. Please note that this is all hardcoded for now
+% and this does not influences the results of the paper as the
+% patellofemoral joint is not investigated. It was not explicitly included
+% in the manual models either.
+% More sophisticated methods are in developement but require more
+% development before we can present them in a publication.
 function     attachPatellaGeom(osimModel, tri_folder, geometry_folder_path, geometry_folder_name, vis_geom_format)
+
 % OpenSim libraries
 dim_fact = 0.001;
+
+% opensim libraries
 import org.opensim.modeling.*
+
+% deals with geometry files
 mesh_file = ['patella_r.',vis_geom_format];
 vis_mesh_file = fullfile(geometry_folder_path, mesh_file);
 cur_tri_geom = load_mesh(fullfile(tri_folder, 'patella_r'));
+
+% writes the geometry for visualisation
 writeOBJfile(cur_tri_geom, vis_mesh_file);
+
+% adds it to the model with appropriate scaling factor
 osim_body = osimModel.getBodySet().get('tibia_r');
 vis_geom = Mesh(fullfile(geometry_folder_name,mesh_file));
 vis_geom.set_scale_factors(Vec3(dim_fact));
